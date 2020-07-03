8c17d5adec9f18f8400ac4926af0bef2133e2a43
xworld2000 <xworld2000@gmail.com>
2020-07-02 20:16:57 -0400

    This commit solves first two challenges

--- xor.js
+++ xor.js
@@ -1,7 +1,14 @@
 function xor(cond1, cond2) {
+
 	// if cond1 is true and cond2 is false return true
-
+	if (cond1 === true && cond2 === false) {
+		return true
+	}
 	// if cond1 is false and cond2 is true return true
-
-	// return false
+	else if (cond1 === false && cond2 === true) {
+		return true
+	}
+	else return false
 }
+
+
