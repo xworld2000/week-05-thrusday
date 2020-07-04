6558640abd7e55fb28514dd88ebc51ef45dc65d8
xworld2000 <xworld2000@gmail.com>
2020-07-04 17:45:50 -0400

    This commit completes flatten challenge

--- flatten.js
+++ flatten.js
@@ -1,9 +1,26 @@
+let arr3 = [];
 function flatten(arr) {
 	// Create a new array
+	let arr1 = [];
 
 	// Loop through every item in the array
 	// If the item is an array, add its items to the new array
+
+	for (let index = 0; index < arr.length; index++) {
+		if (Array.isArray(arr[index])) {
+
+			arr1 = arr1.concat(arr[index]);
+
+		}
+		else arr1.push(arr[index]);
+	}
 	// If not add the item itself to the new array
+	//else { arr1.push(arr[index]); }
 
 	// return the new array
+	arr3 = arr1;
 }
+
+let arr2 = [1, 2, ['nickels', 'dimes', 'pennies', 'dollarbillyall'], 4, 5];
+flatten(arr2);
+console.log(arr3)
